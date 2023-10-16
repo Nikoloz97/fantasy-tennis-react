0c02f4ffcdae87f1297c5ab166e69dcb861c5179
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2023-10-16 18:44:58 -0400

    Cleaned files; created basic homescreen

---
+++ src/UserContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState, useContext } from "react";
+
+// UserContext = allows state to persist/change among different routes
+
+const UserContext = createContext();
+
+export const useUserContext = () => useContext(UserContext);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(undefined);
+  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
+
+  return (
+    <UserContext.Provider
+      value={{ user, setUser, isUserSignedIn, setIsUserSignedIn }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
